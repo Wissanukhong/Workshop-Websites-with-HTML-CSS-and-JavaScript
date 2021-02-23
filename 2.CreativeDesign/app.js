3a8b98f9a15fcc3cced481c5ca63fda71e3bcc51
Wissanu.khong <wissanu.khong@gmail.com>
2021-02-24 04:29:49 +0700

    feat: customize css section-4 on creative design

--- 2.CreativeDesign/app.js
+++ 2.CreativeDesign/app.js
@@ -4,6 +4,3 @@ document.querySelectorAll('.story-btn').forEach((btn) => {
     btn.nextElementSibling.classList.toggle('change');
   });
 });
-
-// document.querySelectorAll('.story-btn') จะทำการเรียก classList แล้ว return เป็น NodeList กลับมาก จากนั้นทำการ forEach
-// ภายใน method forEach สร้าง arroe function ขึ้นมา แล้วทำการ addEventListener
