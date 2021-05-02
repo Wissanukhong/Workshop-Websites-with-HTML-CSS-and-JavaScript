8a8fb3a22667e68915edcbbe008c519ace5cb4af
Wissanu.khong <wissanu.khong@gmail.com>
2021-05-02 22:17:51 +0700

    personal portfolio: make a control work part I

--- 3.PersonalPortfolio/app.js
+++ 3.PersonalPortfolio/app.js
@@ -1,8 +1,29 @@
-// Defind variable to the menu
+// toggle the menu
 const menu = document.querySelector('.menu');
 const navbar = document.querySelector('.navbar');
 
 menu.addEventListener('click', () => {
   navbar.classList.toggle('change');
   menu.classList.toggle('change');
+});
+
+// control the video
+const video = document.querySelector('.video');
+const btn = document.querySelector('.buttons i');
+const bar = document.querySelector('.video-bar');
+
+// function play and pause
+const playPause = () => {
+  if (video.paused) {
+    video.play();
+    btn.className = 'far fa-pause-circle';
+  } else {
+    video.pause();
+    btn.className = 'far fa-play-circle';
+  }
+};
+
+// call the function
+btn.addEventListener('click', () => {
+  playPause();
 });
