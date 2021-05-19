ea44b767efa21b107d4edf5d116e61a8d15782c8
Wissanu.khong <wissanu.khong@gmail.com>
2021-05-19 20:26:15 +0700

    BusinessAgency: function swiper

--- 3.BusinessAgencyWebsite'/app.js
+++ 3.BusinessAgencyWebsite'/app.js
@@ -39,3 +39,18 @@ video.addEventListener('timeupdate', () => {
     btn.className = 'far fa-play-circle';
   }
 });
+
+// swiper
+let swiper = new Swiper('.swiper-container', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 70,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+});
